3835efd8bbd294a9310c7790b177e23d89640f1b
Uchi199 <sasukeitachi234@gmail.com>
2023-09-29 05:23:43 +0530

    Complete game part without db
    Completely done game part except for database saving no match exception is handled in this version

--- Games/Game02.Designer.cs
+++ Games/Game02.Designer.cs
@@ -28,12 +28,177 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.text1 = new System.Windows.Forms.Label();
+            this.text2 = new System.Windows.Forms.Label();
+            this.userInput = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.back = new System.Windows.Forms.Button();
+            this.restart = new System.Windows.Forms.Button();
+            this.scoreLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.start = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.inputName = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // text1
+            // 
+            this.text1.AccessibleName = "text1";
+            this.text1.AutoSize = true;
+            this.text1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text1.Location = new System.Drawing.Point(116, 371);
+            this.text1.Name = "text1";
+            this.text1.Size = new System.Drawing.Size(100, 26);
+            this.text1.TabIndex = 0;
+            this.text1.Text = "Text One";
+            // 
+            // text2
+            // 
+            this.text2.AccessibleName = "text2";
+            this.text2.AutoSize = true;
+            this.text2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.text2.Location = new System.Drawing.Point(519, 371);
+            this.text2.Name = "text2";
+            this.text2.Size = new System.Drawing.Size(99, 26);
+            this.text2.TabIndex = 1;
+            this.text2.Text = "Text Two";
+            // 
+            // userInput
+            // 
+            this.userInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userInput.Location = new System.Drawing.Point(280, 246);
+            this.userInput.Name = "userInput";
+            this.userInput.Size = new System.Drawing.Size(186, 23);
+            this.userInput.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.AccessibleName = "submit";
+            this.button1.Location = new System.Drawing.Point(298, 275);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 37);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(156, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(505, 39);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Find Longest Common Sequnce";
+            // 
+            // back
+            // 
+            this.back.Location = new System.Drawing.Point(15, 203);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(75, 23);
+            this.back.TabIndex = 7;
+            this.back.Text = "Back";
+            this.back.UseVisualStyleBackColor = true;
+            this.back.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // restart
+            // 
+            this.restart.Location = new System.Drawing.Point(12, 275);
+            this.restart.Name = "restart";
+            this.restart.Size = new System.Drawing.Size(75, 23);
+            this.restart.TabIndex = 8;
+            this.restart.Text = "Restart";
+            this.restart.UseVisualStyleBackColor = true;
+            this.restart.Click += new System.EventHandler(this.restart_Click);
+            // 
+            // scoreLabel
+            // 
+            this.scoreLabel.AutoSize = true;
+            this.scoreLabel.Location = new System.Drawing.Point(639, 102);
+            this.scoreLabel.Name = "scoreLabel";
+            this.scoreLabel.Size = new System.Drawing.Size(33, 13);
+            this.scoreLabel.TabIndex = 9;
+            this.scoreLabel.Text = "score";
+            this.scoreLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.scoreLabel.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(622, 203);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "label1";
+            // 
+            // start
+            // 
+            this.start.Location = new System.Drawing.Point(242, 98);
+            this.start.Name = "start";
+            this.start.Size = new System.Drawing.Size(75, 23);
+            this.start.TabIndex = 11;
+            this.start.Text = "Start";
+            this.start.UseVisualStyleBackColor = true;
+            this.start.Click += new System.EventHandler(this.start_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Enter Name";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // inputName
+            // 
+            this.inputName.Location = new System.Drawing.Point(121, 102);
+            this.inputName.Name = "inputName";
+            this.inputName.Size = new System.Drawing.Size(100, 20);
+            this.inputName.TabIndex = 14;
+            this.inputName.TextChanged += new System.EventHandler(this.inputName_TextChanged);
+            // 
+            // Game02
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.inputName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.start);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.scoreLabel);
+            this.Controls.Add(this.restart);
+            this.Controls.Add(this.back);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.userInput);
+            this.Controls.Add(this.text2);
+            this.Controls.Add(this.text1);
+            this.Name = "Game02";
             this.Text = "Game02";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label text1;
+        private System.Windows.Forms.Label text2;
+        private System.Windows.Forms.TextBox userInput;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button back;
+        private System.Windows.Forms.Button restart;
+        private System.Windows.Forms.Label scoreLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button start;
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox inputName;
     }
 }
